09a201691a32fd5b196c6734f737ac5c095afdb1
colehorvitz <colehorvitz@gmail.com>
2020-08-12 13:45:19 -0700

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.9d28c50880dd8ca7b41e2252ae96a153.js"
+  "/precache-manifest.4abbbcf1de57761156da02016b86e7d2.js"
 );
 
 self.addEventListener('message', (event) => {
