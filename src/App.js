a4264c69a92b2eb5ddd775c7021b23b6b16c084b
colehorvitz <colehorvitz@gmail.com>
2020-11-04 12:37:44 -0500

    fixed horizontal width; adjusted scaling for larger devices; began more accessibility

--- src/App.js
+++ src/App.js
@@ -6,16 +6,16 @@ import { PROJECTS } from './projects'
 import { AiFillLinkedin, AiFillGithub } from 'react-icons/ai'
 import { MdEmail } from 'react-icons/md'
 
-class App extends Component { 
+class App extends Component {
   render() {
     return (
       <div>
         <div className="main-wrap">
           <div className="intro-wrap">
             <div className="intro-header">
-              <h1 style={{fontWeight:400}}>cole horvitz</h1>
+              <h1 style={{ fontWeight: 400 }}>cole horvitz</h1>
               <div className="nav">
-                <a href="https://github.com/colehorvitz" target="_blank" rel="noopener noreferrer"><AiFillGithub className="icon"/></a>
+                <a href="https://github.com/colehorvitz" target="_blank" rel="noopener noreferrer"><AiFillGithub className="icon" /></a>
                 <a href="https://www.linkedin.com/in/cole-horvitz-37a58a19a" target="_blank" rel="noopener noreferrer"><AiFillLinkedin className="icon" /></a>
               </div>
             </div>
@@ -23,25 +23,27 @@ class App extends Component {
           </div>
         </div>
         <div className="projects-wrap" id="projects">
-        <h2>projects</h2>
-          <div className="projects-grid">
-            {this.renderProjects()}
+          <div className="projects-subwrap">
+            <h2>projects</h2>
+            <div className="projects-grid">
+              {this.renderProjects()}
+            </div>
           </div>
         </div>
         <div className="footer">
           <div className="icons">
             <a href="https://www.linkedin.com/in/cole-horvitz-37a58a19a" target="_blank" rel="noopener noreferrer"><AiFillLinkedin className="icon" /></a>
-            <a href="mailto:cole_horvitz@brown.edu" target="_blank" rel="noopener noreferrer"><MdEmail className="icon"/></a>
-            <a href="https://github.com/colehorvitz" target="_blank" rel="noopener noreferrer"><AiFillGithub className="icon"/></a>
+            <a href="mailto:cole_horvitz@brown.edu" target="_blank" rel="noopener noreferrer"><MdEmail className="icon" /></a>
+            <a href="https://github.com/colehorvitz" target="_blank" rel="noopener noreferrer"><AiFillGithub className="icon" /></a>
           </div>
         </div>
       </div>
-  );
+    );
   }
 
   renderProjects = () => {
-    return PROJECTS.map( proj => {
-      return <ProjectCard project={proj}/>
+    return PROJECTS.map(proj => {
+      return <ProjectCard project={proj} />
     });
   }
 
