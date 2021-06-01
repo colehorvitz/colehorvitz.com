a6b5449ab61b64d782c5837006b79517805daad0
colehorvitz <colehorvitz@gmail.com>
2021-06-01 11:10:14 -0700

    website tweaks

--- src/App.js
+++ src/App.js
@@ -9,36 +9,67 @@ import { MdEmail } from 'react-icons/md'
 class App extends Component {
   render() {
     return (
-      <div>
-        <div className="main-wrap">
-          <div className="intro-wrap">
+     <div>
+      <div className="wrap">
+        <div className="content">
+          <div className="intro">
             <div className="intro-header">
-              <h1 style={{ fontWeight: 400 }}>cole horvitz</h1>
-              <div className="nav">
-                <a href="https://github.com/colehorvitz" target="_blank" rel="noopener noreferrer"><AiFillGithub className="icon" /></a>
-                <a href="https://www.linkedin.com/in/cole-horvitz-37a58a19a" target="_blank" rel="noopener noreferrer"><AiFillLinkedin className="icon" /></a>
-              </div>
-            </div>
-            <Console />
-          </div>
-        </div>
-        <div className="projects-wrap" id="projects">
-          <div className="projects-subwrap">
-            <h2>projects</h2>
-            <div className="projects-grid">
-              {this.renderProjects()}
+            <h1 style={{ fontWeight: 400 }}>cole horvitz</h1>
+            <div className="nav">
+              <a href="https://github.com/colehorvitz" target="_blank" rel="noopener noreferrer"><AiFillGithub className="icon" /></a>
+              <a href="https://www.linkedin.com/in/cole-horvitz-37a58a19a" target="_blank" rel="noopener noreferrer"><AiFillLinkedin className="icon" /></a>
             </div>
           </div>
+          <Console />
+          </div>
+         <h2>projects</h2>
+         <div className="projects-wrap" id="projects">
+             <div className="projects-grid">
+               {this.renderProjects()}
+             </div>
+         </div>
+       
+      </div>
         </div>
         <div className="footer">
-          <div className="icons">
-            <a href="https://www.linkedin.com/in/cole-horvitz-37a58a19a" target="_blank" rel="noopener noreferrer"><AiFillLinkedin className="icon" /></a>
-            <a href="mailto:cole_horvitz@brown.edu" target="_blank" rel="noopener noreferrer"><MdEmail className="icon" /></a>
-            <a href="https://github.com/colehorvitz" target="_blank" rel="noopener noreferrer"><AiFillGithub className="icon" /></a>
-          </div>
-        </div>
-      </div>
-    );
+           <div className="icons">
+             <a href="https://www.linkedin.com/in/cole-horvitz-37a58a19a" target="_blank" rel="noopener noreferrer"><AiFillLinkedin className="icon" /></a>
+             <a href="mailto:cole_horvitz@brown.edu" target="_blank" rel="noopener noreferrer"><MdEmail className="icon" /></a>
+             <a href="https://github.com/colehorvitz" target="_blank" rel="noopener noreferrer"><AiFillGithub className="icon" /></a>
+           </div>
+         </div> 
+     </div>);
+     
+     
+     
+    //  <div>
+    //     <div className="main-wrap">
+    //       <div className="intro-wrap">
+    //         <div className="intro-header">
+    //           <h1 style={{ fontWeight: 400 }}>cole horvitz</h1>
+    //           <div className="nav">
+    //             <a href="https://github.com/colehorvitz" target="_blank" rel="noopener noreferrer"><AiFillGithub className="icon" /></a>
+    //             <a href="https://www.linkedin.com/in/cole-horvitz-37a58a19a" target="_blank" rel="noopener noreferrer"><AiFillLinkedin className="icon" /></a>
+    //           </div>
+    //         </div>
+    //         <Console />
+    //       </div>
+    //     <div className="projects-wrap" id="projects">
+    //         <div classNamez><h2>projects</h2></div>   
+    //         <div className="projects-grid">
+    //           {this.renderProjects()}
+    //         </div>
+    //     </div>
+    //     <div className="footer">
+    //       <div className="icons">
+    //         <a href="https://www.linkedin.com/in/cole-horvitz-37a58a19a" target="_blank" rel="noopener noreferrer"><AiFillLinkedin className="icon" /></a>
+    //         <a href="mailto:cole_horvitz@brown.edu" target="_blank" rel="noopener noreferrer"><MdEmail className="icon" /></a>
+    //         <a href="https://github.com/colehorvitz" target="_blank" rel="noopener noreferrer"><AiFillGithub className="icon" /></a>
+    //       </div>
+    //     </div>        </div>
+
+    //   </div>
+
   }
 
   renderProjects = () => {
--- src/projects.js
+++ src/projects.js
@@ -1,5 +1,12 @@
 export const PROJECTS = [
     {
+        name: "aloud",
+        desc: "An audio-based social media app.",
+        tags: ["flutter", "dart", "django", "python"],
+        img: "aloud.png",
+        url:"https://www.aloudaudio.app",
+    },
+    {
         name: "Brown University Bear Dens",
         desc: "A web application that has helped over 4000 Brown students discover on-campus housing.",
         tags: ["react", "css", "sparql"],
@@ -22,12 +29,11 @@ export const PROJECTS = [
         img: "doc.jpg",
         url: "https://www.deepread.app",
     },
-
     {
         name: "Spidr Dev",
         desc: "A web design project that has developed websites for various clients.",
         tags: ["html", "css"],
         img: "design1.jpg",
-        url:"https://spidr.dev",
+        url:"https://www.spidr.dev",
     },
 ]
